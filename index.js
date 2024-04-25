b147d583e9bd6a3e827a410f953efd3e4b7214be
Mickellan <160796377+Mickellan@users.noreply.github.com>
2024-04-24 20:01:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,39 +3,39 @@ async function runExample() {
     var x = [];
 
     // Retrieve input values from HTML input elements
-    x[0] = document.querySelector('.school:checked').value || '';
-    x[1] = document.querySelector('.sex:checked').value || '';
-    x[2] = document.querySelector('.age').value || '';
-    x[3] = document.querySelector('.address:checked').value || '';
-    x[4] = document.querySelector('.famsize:checked').value || '';
-    x[5] = document.querySelector('.Pstatus:checked').value || '';
-    x[6] = document.querySelector('.Medu').value || '';
-    x[7] = document.querySelector('.Fedu').value || '';
-    x[8] = document.querySelector('.Mjob').value || '';
-    x[9] = document.querySelector('.Fjob').value || '';
-    x[10] = document.querySelector('.reason').value || '';
-    x[11] = document.querySelector('.guardian:checked').value || '';
-    x[12] = document.querySelector('.traveltime').value || '';
-    x[13] = document.querySelector('.studytime').value || '';
-    x[14] = document.querySelector('.failures').value || '';
-    x[15] = document.querySelector('.schoolsup:checked').value || '';
-    x[16] = document.querySelector('.famsup:checked').value || '';
-    x[17] = document.querySelector('.paid:checked').value || '';
-    x[18] = document.querySelector('.activities:checked').value || '';
-    x[19] = document.querySelector('.nursery:checked').value || '';
-    x[20] = document.querySelector('.higher:checked').value || '';
-    x[21] = document.querySelector('.internet:checked').value || '';
-    x[22] = document.querySelector('.romantic:checked').value || '';
-    x[23] = document.querySelector('.famrel').value || '';
-    x[24] = document.querySelector('.freetime').value || '';
-    x[25] = document.querySelector('.goout').value || '';
-    x[26] = document.querySelector('.Dalc').value || '';
-    x[27] = document.querySelector('.Walc').value || '';
-    x[28] = document.querySelector('.health').value || '';
-    x[29] = document.querySelector('.absences').value || '';
-    x[30] = document.querySelector('.G1').value || '';
-    x[31] = document.querySelector('.G2').value || '';
-
+    x[0] = document.getElementById('box1').value;
+    x[1] = document.getElementById('box2').value;
+    x[2] = document.getElementById('box3').value;
+    x[3] = document.getElementById('box4').value;
+    x[4] = document.getElementById('box5').value;
+    x[5] = document.getElementById('box6').value;
+    x[6] = document.getElementById('box7').value;
+    x[7] = document.getElementById('box8').value;
+    x[8] = document.getElementById('box9').value;
+    x[9] = document.getElementById('box10').value;
+    x[10] = document.getElementById('box11').value;
+    x[11] = document.getElementById('box12').value;
+    x[12] = document.getElementById('box13').value;
+    x[13] = document.getElementById('box14').value;
+    x[14] = document.getElementById('box15').value;
+    x[15] = document.getElementById('box16').value;
+    x[16] = document.getElementById('box17').value;
+    x[17] = document.getElementById('box18').value;
+    x[18] = document.getElementById('box19').value;
+    x[19] = document.getElementById('box20').value;
+    x[20] = document.getElementById('box21').value;
+    x[21] = document.getElementById('box22').value;
+    x[22] = document.getElementById('box23').value;
+    x[23] = document.getElementById('box24').value;
+    x[24] = document.getElementById('box25').value;
+    x[25] = document.getElementById('box26').value;
+    x[26] = document.getElementById('box27').value;
+    x[27] = document.getElementById('box28').value;
+    x[28] = document.getElementById('box29').value;
+    x[29] = document.getElementById('box30').value;
+    x[30] = document.getElementById('box31').value;
+    x[31] = document.getElementById('box32').value;
+    x[32] = document.getElementById('box33').value;
 
 
     // Create a Float32Array from the input values
